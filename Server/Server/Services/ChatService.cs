c71143efe5bc51efdae4bfed40a0e3e85cb9a911
Andrey Kondratov <andrew.kondratov@gmail.com>
2016-10-16 21:05:36 +0300

    Added a service host application.

--- Server/Server/Services/ChatService.cs
+++ Server/Server/Services/ChatService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.ServiceModel;
 using Andead.Chat.Resources.Resources.Strings;
 
 namespace Andead.Chat.Server
---
+++ Server/ServiceHost/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Andead.Chat.Server.Wcf;
+
+namespace ServiceHost
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            System.ServiceModel.ServiceHost host = null;
+            try
+            {
+                host = new System.ServiceModel.ServiceHost(typeof(Service));
+
+                // Start
+                host.Open();
+
+                Console.WriteLine(
+                    $"The service is ready at {host.BaseAddresses.Select(uri => uri.ToString()).Aggregate((s, s1) => s + ", " + s1)}.");
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+            }
+            catch (Exception exception)
+            {
+                host = null;
+                Console.WriteLine("Service can not be started \n\nError Message [" + exception.Message + "]");
+            }
+            finally
+            {
+                // Stop
+                host?.Close();
+            }
+        }
+    }
+}
