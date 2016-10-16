ce1acf87c3e67d95c07a3fd1af577060bc26384b
Andrey Kondratov <andrew.kondratov@gmail.com>
2016-10-16 23:18:46 +0300

    Added some comments.

--- Clients/Client/Interfaces/IServiceClient.cs
+++ Clients/Client/Interfaces/IServiceClient.cs
@@ -6,20 +6,46 @@ namespace Andead.Chat.Client.Interfaces
 {
     public interface IServiceClient : IDisposable
     {
+        /// <summary>
+        ///     Gets a boolean value indicating that the client is currently signed in to server.
+        /// </summary>
         bool SignedIn { get; }
 
+        /// <summary>
+        ///     Gets the server name.
+        /// </summary>
         string ServerName { get; }
 
-        Task<int?> GetOnlineCountAsync();
-
+        /// <summary>
+        ///     Performs sign-in with a specified username asynchronously.
+        /// </summary>
+        /// <param name="name">Username.</param>
         Task<SignInResult> SignInAsync(string name);
 
-        event EventHandler<MessageReceivedEventArgs> MessageReceived;
-
+        /// <summary>
+        ///     Performs sign-out asynchronously.
+        /// </summary>
         Task SignOutAsync();
 
+        /// <summary>
+        ///     Gets the number of online users asynchronously.
+        /// </summary>
+        Task<int?> GetOnlineCountAsync();
+
+        /// <summary>
+        ///     Gets the names of online users asynchronously.
+        /// </summary>
+        /// <returns></returns>
+        Task<string[]> GetNamesOnlineAsync();
+
+        /// <summary>
+        ///     Sends message to the chat asynchronously.
+        /// </summary>
         Task<SendMessageResult> SendAsync(string message);
 
-        Task<string[]> GetNamesOnlineAsync();
+        /// <summary>
+        ///     Raises when a message is received.
+        /// </summary>
+        event EventHandler<MessageReceivedEventArgs> MessageReceived;
     }
 }
